94de6dad41c0b8615f7b68d94f5b16d6dfeea24b
李博修 <liboxiu@dreamofalice.club>
2021-03-19 16:30:31 +0800

    ✨ feat: log grpc service

--- main.go
+++ main.go
@@ -88,6 +88,7 @@ func (*server) Exist(ctx context.Context,
 func (*server) GetUserMsgs(ctx context.Context, req *logservice.ListRequest) (arr *logservice.LogArray, err error) {
 	cur, err := col.Find(ctx, bson.M{"operation_target_owner_id": req.UserId},
 		options.Find().SetSkip(int64(req.Page)*int64(req.Pagesize)).SetLimit(int64(req.Pagesize)).SetSort(bson.M{"update_time": -1}))
+	col.UpdateMany(ctx, bson.M{"operation_target_owner_id": req.UserId}, bson.D{{"$set", bson.M{"processed": true}}})
 	if err != nil {
 		return nil, err
 	}
@@ -106,7 +107,10 @@ func (*server) GetUserMsgs(ctx context.Context, req *logservice.ListRequest) (ar
 	arr.Logs = logs
 	return
 }
-
+func (*server) GetUserNewMsgNum(ctx context.Context, uid *logservice.UserID) (num *logservice.Num, err error) {
+	num.Num, err = col.CountDocuments(ctx, bson.M{"operation_target_owner_id": uid.UserId})
+	return
+}
 func main() {
 	lis, err := net.Listen("tcp", os.Getenv("LOG_PORT"))
 	if err != nil {
