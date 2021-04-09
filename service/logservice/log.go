9b3ad999d5fb0b318db9cbbe513acc8557b05266
李博修 <liboxiu@dreamofalice.club>
2021-04-09 22:19:25 +0800

    🐛 [fix] addd export types

--- service/logservice/log.go
+++ service/logservice/log.go
@@ -8,6 +8,15 @@ import (
 	grpc "google.golang.org/grpc"
 )
 
+type LogClient interface {
+	LogServiceClient
+	SendCountQuery(ctx context.Context, query bson.D, opts ...grpc.CallOption) (num *Num, err error)
+}
+
+func BuildLogClient(cc grpc.ClientConnInterface) LogClient {
+	return &logServiceClient{cc}
+}
+
 func (c *logServiceClient) SendCountQuery(ctx context.Context, query bson.D, opts ...grpc.CallOption) (num *Num, err error) {
 	in := &Query{}
 	in.Query, err = json.Marshal(query)
